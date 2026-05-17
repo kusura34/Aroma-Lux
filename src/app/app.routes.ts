0ae5f87cd20e4e91f34d130c518c58e2787aecb4
Rekurt-116 <sabrlitet@gmail.com>
2026-05-17 20:07:18 +0300

    layout + header + home-banners

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from '../layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from '../layout/admin-layout/admin-layout.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { ProductsListComponent } from '../pages/products-list/products-list.component';
+import { CartComponent } from '../pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainLayoutComponent,
+    children: [
+        {path: '', component: HomeComponent},
+        {path: 'products', component: ProductsListComponent},
+        {path: 'cart', component: CartComponent},
+    ]
+   },
+  { path: 'admin', component: AdminLayoutComponent },
+];
