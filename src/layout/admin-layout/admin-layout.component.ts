02c3a2da8822ac88e058c14c7f542018f532150c
Rekurt-116 <sabrlitet@gmail.com>
2026-06-01 20:33:31 +0300

    feat(admin): layout admin panel and integrate core components

--- src/layout/admin-layout/admin-layout.component.ts
+++ src/layout/admin-layout/admin-layout.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
